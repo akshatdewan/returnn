27fbe765f01f1b168c7410bf0c09ec5cb6ab66cf
Akshat Dewan <akshatdewan@gmail.com>
2019-02-06 14:31:24 +0000

    Minor bug

--- TFEngine.py
+++ TFEngine.py
@@ -2198,7 +2198,8 @@ class Engine(object):
     input_audio_feature_extractor = None
     output_data = self.network.extern_data.get_default_target_data()
     output_vocab = output_data.vocab
-    if isinstance(self.config.typed_dict.get("dev", None), dict) and self.config.typed_dict["dev"]["class"] == "LibriSpeechCorpus":
+    print(self.config.typed_dict.get("dev", None))
+    if isinstance(self.config.typed_dict.get("dev", None), dict) and self.config.typed_dict["dev"]["class"] == "LibriWipoEUCorpus":
       # A bit hacky. Assumes that this is a dataset description for e.g. LibriSpeechCorpus.
       import soundfile  # pip install pysoundfile
       bpe_opts = self.config.typed_dict["dev"]["bpe"]
